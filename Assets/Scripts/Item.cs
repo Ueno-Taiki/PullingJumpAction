44a65be3f1edea8fe6673f95cad6a66ef87940eb
上野太己 <k023g0051@g.neec.ac.jp>
2024-06-10 11:07:46 +0900

    接触判定

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        DestroySelf();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
