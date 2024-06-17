2fb2d83ca1dc49137754a1b2aaae926a1148f60c
上野太己 <k023g0051@g.neec.ac.jp>
2024-06-17 13:05:18 +0900

    サウンド・シーンを追加した

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    private Animator animator;
+    private AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,12 +22,13 @@ public class Item : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Enter");
+        animator.SetTrigger("Get");
+        audioSource.Play();
     }
 
     private void OnTriggerStay(Collider other)
     {
-        DestroySelf();
+
     }
 
     private void OnTriggerExit(Collider other)
